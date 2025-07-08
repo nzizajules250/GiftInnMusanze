0fd1cf776c1fcdd5c7c11ebb3b69d567944fc381
Nziza Jules <nzizajules250@gmail.com>
2025-07-08 07:13:41 +0000

    run npm run build and see if there are other errors

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import * as React from "react"
-import { useToast } from "@/hooks/use-toast"
 import {
   Toast,
   ToastClose,
@@ -10,18 +8,10 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
+import { useToast } from "@/hooks/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
-  const [isMounted, setIsMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setIsMounted(true)
-  }, [])
-
-  if (!isMounted) {
-    return null
-  }
 
   return (
     <ToastProvider>
